d5e81c8d4d457fc0796d30735d4e437f90c37f72
S-tanishi <66398847+S-tanishi@users.noreply.github.com>
2020-09-17 11:57:29 +0900

    add:item_listをviewに渡す

--- app/Http/Controllers/EntryController.php
+++ app/Http/Controllers/EntryController.php
@@ -10,10 +10,18 @@ class EntryController extends Controller
     public function index()
     {   //投稿一覧画面表示
         dd(Entry::all());
+
+        $item_list = Entry::all();
+
+        return view("list", [
+            "item_list" => $item_list
+        ]);
     }
 
     public function create()
     {   //投稿処理
         echo "create";
     }
+
+    
 }
